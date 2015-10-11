12bdda8c116ce6fa23987250787ec8f2a1a60de7
Paul D Barham <paulb@savagesoft.com>
2015-10-10 22:12:15 -0500

    Added mockup of new form

--- vendor/CommonAccord/cmacc/library/openedit.php
+++ vendor/CommonAccord/cmacc/library/openedit.php
@@ -25,9 +25,9 @@
         body {
             padding-top: 50px;
         }
+
         .starter-template {
             padding: 40px 15px;
-            text-align: center;
         }
     </style>
 
@@ -50,14 +50,13 @@ ini_set("allow_url_include", true);
 
 $lib_path = LIB_PATH;
 $document = `perl $lib_path/openedit-parser.pl $path/$dir`;
+$document .= "\nWAS=" . date("Y/m/d") . " : " . time() . "\n\n";
+$document .= file_get_contents($path . $dir, FILE_USE_INCLUDE_PATH);
+
 
 include("$lib_path/view-tabs-bootstrap.php");
 
 ?>
-
-
-
-
 
 <div class="container">
 
@@ -65,26 +64,96 @@ include("$lib_path/view-tabs-bootstrap.php");
         <h1><?php echo $dir; ?></h1>
 
         <div id="tab-edit">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <textarea id="textedit" <?php echo TEXT_EDIT_WINDOW_SIZE; ?> name="newcontent"
+                          style="<?php echo TEXT_EDIT_AREA_STYLE; ?>">
 
-            <?php
-            echo "<form action=$_SERVER[PHP_SELF] method='post'>
-        <textarea id='textedit' " . TEXT_EDIT_WINDOW_SIZE . " name='newcontent' style='" . TEXT_EDIT_AREA_STYLE . "'>";
-
-            echo $document;
-
-            echo "\nWAS=" . date("Y/m/d") . " : " . time() . "\n\n";
-
-            echo file_get_contents($path . $dir, FILE_USE_INCLUDE_PATH);
-            ?>
-
-            </textarea><br>
-            <input class="btn btn-info" type="submit" name="submit" value="Save">
-            <input type="hidden" name="file" value="' . $dir . '">
-            <input type="hidden" name="action" value="source">
+<?php echo $document; ?>
+                </textarea><br>
+                <input class="btn btn-info" type="submit" name="submit" value="Save">
+                <input type="hidden" name="file" value="<?php echo $dir; ?>">
+                <input type="hidden" name="action" value="source">
             </form>
 
 
         </div>
+    </div>
+
+    <div class="row">
+
+        <form class="form-horizontal">
+            <fieldset>
+
+                <!-- Form Name -->
+                <legend>CMACC FORM</legend>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-3 control-label" for="Ti">Title</label>
+
+                    <div class="col-md-9">
+                        <input id="Ti" name="Ti" placeholder="Enter: Title of Document" class="form-control input-md"
+                               required="" type="text">
+                        <span class="help-block">help</span>
+                    </div>
+                </div>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-3 control-label" for="Conf.Exclude.Xref">Conf.Exclude.Xref</label>
+
+                    <div class="col-md-9">
+                        <input id="Conf.Exclude.Xref" name="Conf.Exclude.Xref" placeholder="placeholder"
+                               class="form-control input-md" required="" type="text">
+                        <span class="help-block">Help text</span>
+                    </div>
+                </div>
+
+                <!-- Multiple Radios -->
+                <div class="form-group">
+                    <label class="col-md-3 control-label" for="radios">Obligations</label>
+
+                    <div class="col-md-9">
+                        <div class="radio">
+                            <label for="radios-0">
+                                <input name="radios" id="radios-0" value="1" checked="checked" type="radio">
+                                {The_Receiving_Party}'s obligations with respect to all
+                                {Confidential_Information_of_the_Disclosing_Party} will terminate only pursuant to
+                                {Conf.Exclude.Xref}.
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label for="radios-1">
+                                <input name="radios" id="radios-1" value="2" type="radio">
+                                {The_Receiving_Party}'s obligations with respect to all
+                                {Confidential_Information_of_the_Disclosing_Party} will terminate on the earlier of
+                                {Confidentiality.End.YMD} or when all such information has become subject to an
+                                exclusion from confidentiality pursuant to {Conf.Exclude.Xref}.
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label for="radios-2">
+                                <input name="radios" id="radios-2" value="3" type="radio">
+                                {The_Receiving_Party}'s obligations with respect to each element of
+                                {Confidential_Information_of_the_Disclosing_Party} will terminate on the earlier of
+                                {DurationOfConfidentiality} after the time such element was communicated by
+                                {the_Disclosing_Party} to {the_Receiving_Party} or when all such information has become
+                                subject to an exclusion from confidentiality pursuant to {Conf.Exclude.Xref}.
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Button (Double) -->
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <button id="button1id" name="button1id" class="btn btn-primary">Save</button>
+                        <a href="#" id="button2id" name="button2id" class="btn btn-danger">Cancel</a>
+                    </div>
+                </div>
+
+            </fieldset>
+        </form>
     </div>
 </div>
 
