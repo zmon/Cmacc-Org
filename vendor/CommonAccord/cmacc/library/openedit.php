04d7022d034492a789598582d2cba561cf10edbc
Paul D Barham <paulb@savagesoft.com>
2015-10-13 21:44:35 -0500

    Now creates text input fields from cmacc document
    Has two stub 'user defined' fields.

--- vendor/CommonAccord/cmacc/library/openedit.php
+++ vendor/CommonAccord/cmacc/library/openedit.php
@@ -56,6 +56,131 @@ $document .= file_get_contents($path . $dir, FILE_USE_INCLUDE_PATH);
 
 include("$lib_path/view-tabs-bootstrap.php");
 
+
+class Screen
+{
+    var $cmacc_fields = array();                                        // Fields from cmacc
+    var $cmacc_id = 0;                                                  // Index or row number;
+
+    var $fields = array();                                              // Fields defined by the user
+
+    function __construct()
+    {
+        $this->fields[] = array(
+            'name' => 'Ti',
+            'label' => 'Title',
+            'value' => 'Term of Confidentiality',
+            'place_holder' => 'Enter: Title to Document',
+            'type' => 'text',
+            'required' => '',
+            'cmacc_id' => 0
+        );
+        $this->fields[] = array(
+            'name' => 'SecName',
+            'label' => 'Section Name',
+            'value' => '',
+            'place_holder' => 'Enter: Section Title',
+            'type' => 'text',
+            'required' => '',
+            'cmacc_id' => 0
+        );
+
+    }
+
+    function add_cmacc_field($name, $value)
+    {
+
+        $name = trim($name);                                            //  Remove any extra white space
+
+        $this->cmacc_id++;
+        $this->cmacc_fields[$this->cmacc_id] = array(
+            'name' => $name,
+            'label' => $name,
+            'value' => $value
+        );
+
+        $field_offset = $this->get_field_offset($name);
+
+        if ($field_offset === FALSE) {
+            $field_offset = $this->add_field($name, $name, $value);
+        }
+
+        $this->fields[$field_offset]['cmacc_id'] = $this->cmacc_id;
+
+    }
+
+    function get_field_offset($name)
+    {
+
+        foreach ($this->fields AS $i => $v) {
+            if ($v['name'] == $name) return $i;
+        }
+
+        return false;
+    }
+
+    function add_field($name, $label = '', $value = '', $place_holder = '', $type = 'text', $required = '', $cmacc_id = 0)
+    {
+        $this->fields[] = array(
+            'name' => $name,
+            'label' => $label,
+            'value' => $value,
+            'place_holder' => $place_holder,
+            'type' => $type,
+            'required' => $required,
+            'cmacc_id' => $cmacc_id
+        );
+
+        return sizeof($this->fields) - 1;
+
+    }
+
+    function dump()
+    {
+        var_dump($this->fields);
+        var_dump($this->cmacc_fields);
+    }
+
+    function paint_fields()
+    {
+
+        $html = '';
+
+        foreach ($this->fields AS $i => $v) {
+
+            $name = $v['name'];
+            $value = $v['value'];
+            $label = $v['label'];
+            $place_holder = $v['place_holder'];
+            $type = $v['type'];
+            $required = $v['required'];
+
+            if (empty($name)) continue;
+
+            $f = <<<EOM
+                <div class="form-group">
+                    <label class="col-md-3 control-label" for="$name">$label</label>
+
+                    <div class="col-md-9">
+                        <input id="$name" name="$name" placeholder="$place_holder"
+                               class="form-control input-md" required="" type="text" value="$value">
+                    </div>
+                </div>
+EOM;
+
+
+            $html .= $f;
+
+        }
+
+        return $html;
+    }
+
+
+}
+
+$screen = new Screen();
+
 ?>
 
 <div class="container">
@@ -69,6 +194,9 @@ include("$lib_path/view-tabs-bootstrap.php");
                           style="<?php echo TEXT_EDIT_AREA_STYLE; ?>">
 
 <?php echo $document; ?>
+
+
+
                 </textarea><br>
                 <input class="btn btn-info" type="submit" name="submit" value="Save">
                 <input type="hidden" name="file" value="<?php echo $dir; ?>">
@@ -79,6 +207,27 @@ include("$lib_path/view-tabs-bootstrap.php");
         </div>
     </div>
 
+
+    <?php
+    $lines = explode("\n", $document);
+
+    foreach ($lines AS $line) {
+        preg_match("/(.*)=(.*)/", $line, $field);
+
+        if (sizeof($field) == 0) continue;             // skip blank lines
+
+        $field_name = $field[1];
+        $field_value = $field[2];
+
+
+        $screen->add_cmacc_field($field_name, $field_value);
+
+
+    }
+
+    ?>
+
+
     <div class="row">
 
         <form class="form-horizontal">
@@ -87,27 +236,11 @@ include("$lib_path/view-tabs-bootstrap.php");
                 <!-- Form Name -->
                 <legend>CMACC FORM</legend>
 
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-3 control-label" for="Ti">Title</label>
+                <hr>
 
-                    <div class="col-md-9">
-                        <input id="Ti" name="Ti" placeholder="Enter: Title of Document" class="form-control input-md"
-                               required="" type="text">
-                        <span class="help-block">help</span>
-                    </div>
-                </div>
+                <?php echo $screen->paint_fields(); ?>
 
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-3 control-label" for="Conf.Exclude.Xref">Conf.Exclude.Xref</label>
-
-                    <div class="col-md-9">
-                        <input id="Conf.Exclude.Xref" name="Conf.Exclude.Xref" placeholder="placeholder"
-                               class="form-control input-md" required="" type="text">
-                        <span class="help-block">Help text</span>
-                    </div>
-                </div>
+                <hr>
 
                 <!-- Multiple Radios -->
                 <div class="form-group">
